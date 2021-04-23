004a92a8823679506eca490676fa91cc8f58b633
Jeffrey Saelee <gwfds123@gmail.com>
2021-04-23 01:03:30 -0700

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+    Private MainRect As Rectangle
+    Private Hero As Player
+    Private keysPressed As New HashSet(Of Keys)
+    Private counter As Integer = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DoubleBuffered = True
+        StartPlayer()
+    End Sub
+
+    Private Sub StartPlayer()
+        MainRect = DisplayRectangle
+        Hero = New Player(MainRect)
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        Dim G As Graphics = e.Graphics
+        G.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+        Hero.Show(G)
+        Hero.Update()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If keysPressed.Contains(Keys.A) And Hero.speedX > -Hero.maxSpeed Then
+            Hero.speedX -= 1
+            Hero.moving = True
+        End If
+        If keysPressed.Contains(Keys.D) And Hero.speedX < Hero.maxSpeed Then
+            Hero.speedX += 1
+            Hero.moving = True
+        End If
+        If keysPressed.Contains(Keys.W) And Hero.speedY > -Hero.maxSpeed Then
+            Hero.speedY -= 1
+            Hero.moving = True
+        End If
+        If keysPressed.Contains(Keys.S) And Hero.speedY < Hero.maxSpeed Then
+            Hero.speedY += 1
+            Hero.moving = True
+        End If
+        If counter Mod 1 = 0 And Hero.moving And Hero.Direction < 35 Then
+            Hero.Direction += 1
+            Debug.WriteLine(Hero.Direction)
+        End If
+        Invalidate()
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        keysPressed.Add(e.KeyCode)
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        keysPressed.Remove(e.KeyCode)
+    End Sub
+End Class
---
+++ Player.vb
@@ -0,0 +1,89 @@
+﻿Public Class Player
+    Public Property x As Double
+    Public Property y As Double
+    Public Property speedX As Decimal
+    Public Property speedY As Decimal
+    Public Property visible As Boolean
+    Public Property moving As Boolean
+
+    Public Property maxSpeed As Decimal
+    Private direct As Integer
+
+    Public Property Direction As Integer
+        Get
+            Return direct
+        End Get
+        Set(value As Integer)
+            direct = value
+        End Set
+    End Property
+
+    Private frame(35) As Bitmap
+
+    Private MainRect As Rectangle
+
+    Sub New(MainRect As Rectangle)
+        Me.MainRect = MainRect
+        _x = MainRect.Width / 2 - 45
+        _y = MainRect.Height / 2 - 45
+        _maxSpeed = 1
+        Direction = 18
+        moving = False
+        Dim img As New Bitmap(My.Resources.playerwalking)
+        _visible = True
+        Dim bitY = 0
+
+        For i As Integer = 0 To 35
+            frame(i) = New Bitmap(61, 64)
+            Dim gr As Graphics = Graphics.FromImage(frame(i))
+            Dim xPosition As Integer = (i Mod 9) * 63
+            gr.DrawImage(img, 0, 0, New RectangleF(xPosition, Int(i / 9) * 64, 61, 64), GraphicsUnit.Pixel)
+        Next
+    End Sub
+
+    Public Sub Show(G As Graphics)
+
+        If visible Then
+            G.DrawImage(CType(frame(direct), Image), CSng(_x), CSng(_y), 61, 64)
+        End If
+    End Sub
+
+    Public Sub Update()
+        _x += _speedX
+        _y += _speedY
+        declerate()
+    End Sub
+
+    Public Sub declerate()
+        If _speedY > 0 Then
+            _speedY -= 0.2
+            If _speedX = 0 Then
+                If Direction < 18 Or Direction > 26 Then
+                    Direction = 18
+                End If
+            End If
+        ElseIf _speedY < 0 Then
+            _speedY += 0.2
+            If _speedX = 0 Then
+                If Direction < 0 Or Direction > 7 Then
+                    Direction = 0
+                End If
+            End If
+        End If
+        If _speedX > 0 Then
+            _speedX -= 0.2
+            If Direction < 27 Or Direction > 34 Then
+                Direction = 27
+            End If
+        ElseIf _speedX < 0 Then
+            _speedX += 0.2
+            If Direction < 9 Or Direction > 17 Then
+                Direction = 9
+            End If
+        End If
+        If _speedY = 0 Or _speedX = 0 Then
+            moving = False
+        End If
+
+    End Sub
+End Class
